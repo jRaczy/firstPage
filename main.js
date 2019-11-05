5859545b98efae7a063c003b256ecaa5007cfba1
Jan Raczy <janraczy@gmail.com>
2019-11-05 21:13:06 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,4 @@
+console.log('Witaj w najostrzejszej pizzeri w polsce');
+const pizzeriaName='Frank Company';
+const owners ='John and Kate Frankowscy';
+console.log(`Nazwa pizzeri: ${pizzeriaName} i jej właścicielami są: ${owners}`);
